50986898f075c65e5149937befb48f6dc3bd303e
Tim <tc689@drexel.edu>
2017-09-14 01:34:35 -0400

    Basic Discord Bot
    Requires Discord.io and watson to be installed

---
+++ bot.js
@@ -0,0 +1,54 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+var ref = require('./grumpsquotes.json');
+
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+	colorize: true
+});
+logger.level = 'debug';
+
+var bot = new Discord.Client({
+	token: auth.token,
+	autorun: true
+});
+
+bot.on('ready', function(evt){
+	logger.info('Connected');
+	logger.info('Logged in as: ');
+	logger.info(bot.username+' - ('+bot.id+')');
+});
+
+bot.on('message', function(user, userID, channelID, message, evt){
+	if(message.substring(0, 1) == '<'){
+		var args = message.substring(1).split(' ');
+		var cmd = args[0];
+
+		args = args.splice(1);
+		switch(cmd){
+			case 'help':
+			case 'list':
+			case 'commands':
+				bot.sendMessage({
+					to: channelID,
+					message: ref.commands
+				});
+			break;
+			case 'ping':
+				bot.sendMessage({
+					to: channelID,
+					message: 'Pong!'
+				});
+			break;
+			case 'gq':
+			case 'gquote':
+				quotes = ref.quotes
+				bot.sendMessage({
+					to: channelID,
+					message: quotes[Math.floor(Math.random()*quotes.length)]
+				})
+			break;
+		}
+	}
+})
